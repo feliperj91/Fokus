d49cc3f495bdc17b3d426d64115f476517ac2742
Felipe Almeida <fcalmeida23@gmail.com>
2024-05-17 13:08:26 -0300

    Fokus

---
+++ script.js
@@ -0,0 +1,116 @@
+const html = document.querySelector('html')
+const btnFoco = document.querySelector('.app__card-button--foco')
+const btnCurto = document.querySelector('.app__card-button--curto')
+const btnLongo = document.querySelector('.app__card-button--longo')
+const startPauseBt = document.querySelector('#start-pause')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const beep = new Audio('/sons/beep.mp3')
+const play = new Audio('/sons/play.wav')
+const pause = new Audio('/sons/pause.mp3')
+musica.loop = true;
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
+
+
+btnFoco.addEventListener('click', () => {
+    alterarContexto('foco')
+    btnFoco.classList.add('active')
+
+})
+
+btnCurto.addEventListener('click', () => {
+    alterarContexto('descanso-curto')
+    btnCurto.classList.add('active')
+
+
+})
+
+btnLongo.addEventListener('click', () => {
+    alterarContexto('descanso-longo')
+    btnLongo.classList.add('active')
+})
+
+function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superficie.
+            <strong class="app__title-strong">faça uma pausa longa.</strong>            
+            `
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        zerar()
+        beep.play()
+        alert('Tempo Finalizado!')
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+}
+
+
+
+
+function iniciarOuPausar() {
+    if (intervaloId == null){
+        play.play()
+        intervaloId = setInterval(contagemRegressiva, 1000)
+    }
+    else if (intervaloId != null) {
+        pause.play()
+        zerar()
+        return
+    }
+    
+    
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
+}
+
+function alterarFoco(contexto) {
+
+}
