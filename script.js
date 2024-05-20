3af940a7dc36d810ae99f8231b377113b69a0b94
Felipe Almeida <fcalmeida23@gmail.com>
2024-05-20 18:26:43 -0300

    Ajuste na definição dos tempos e comentários

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const btnFoco = document.querySelector('.app__card-button--foco')
 const btnCurto = document.querySelector('.app__card-button--curto')
 const btnLongo = document.querySelector('.app__card-button--longo')
 const startPauseBt = document.querySelector('#start-pause')
+const startPauseBotao = document.querySelector('#start-pause span')
+const imgStartPausebt = document.querySelector('.app__card-primary-butto-icon')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const tempoNaTela = document.querySelector('#timer')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
 const beep = new Audio('/sons/beep.mp3')
@@ -13,9 +16,10 @@ const play = new Audio('/sons/play.wav')
 const pause = new Audio('/sons/pause.mp3')
 musica.loop = true;
 
-let tempoDecorridoEmSegundos = 5
-let intervaloId = null
+let tempoDecorridoEmSegundos = 1500 //Define o tempo inicial em 1500ms
+let intervaloId = null //Variavel responsável por definit o intervalo como nulo
 
+//Pausa o contador caso este esteja em processo de contagem e inicia caso esteja pausado, almém de executar os respectivos audios.
 musicaFocoInput.addEventListener('change', () => {
     if (musica.paused) {
         musica.play()
@@ -24,26 +28,33 @@ musicaFocoInput.addEventListener('change', () => {
     }
 })
 
-
+//Altera o contexto da pagina quando clicado no botão foco
 btnFoco.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
     btnFoco.classList.add('active')
 
 })
 
+//Altera o contexto da pagina quando clicado no botão descanso curto
 btnCurto.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
     btnCurto.classList.add('active')
 
 
 })
 
+//Altera o contexto da pagina quando clicado no botão descanso longo
 btnLongo.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
     btnLongo.classList.add('active')
 })
 
+//Função responsávem por mostrar os tempos, alterar os textos e imagens de acordo com o contexto
 function alterarContexto(contexto) {
+    mostrarTempo()
     botoes.forEach(function (contexto) {
         contexto.classList.remove('active')
     })
@@ -74,6 +85,7 @@ function alterarContexto(contexto) {
 
 }
 
+//Caso o contador seja menor ou igual a 0, é iniciado o audio beep e exibido o alerta de Tempo Finalizado
 const contagemRegressiva = () => {
     if (tempoDecorridoEmSegundos <= 0) {
         zerar()
@@ -82,30 +94,35 @@ const contagemRegressiva = () => {
         return
     }
     tempoDecorridoEmSegundos -= 1
-    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+    mostrarTempo()
 }
 
 
 
-
+//Função responsável por iniciar o contador, caso esse esteja zerado, além de reproduzir os audios e alterar o texto de Começar para Pausar
 function iniciarOuPausar() {
-    if (intervaloId == null){
+    if (intervaloId == null) {
         play.play()
+        startPauseBotao.textContent = 'Pausar'
+        imgStartPausebt.setAttribute('src', '/imagens/pause.png')
         intervaloId = setInterval(contagemRegressiva, 1000)
     }
     else if (intervaloId != null) {
         pause.play()
+        startPauseBotao.textContent = 'Começar'
+        imgStartPausebt.setAttribute('src', '/imagens/play_arrow.png')
         zerar()
         return
     }
-    
-    
+
+
 }
 
+//Evento de click que aciona a função IniciarOuPausar
 startPauseBt.addEventListener('click', iniciarOuPausar)
 
 
-
+//Função que zera o contador utilizando clearInterval
 function zerar() {
     clearInterval(intervaloId)
     intervaloId = null
@@ -114,3 +131,12 @@ function zerar() {
 function alterarFoco(contexto) {
 
 }
+//Exibe o tempo do contador de acordo com o formato definido
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-br', { minute: '2-digit', second: '2-digit' })
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+
+}
+
+mostrarTempo()
